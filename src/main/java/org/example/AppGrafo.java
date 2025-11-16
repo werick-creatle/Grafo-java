0a6cdd2d6c7f1d78d8fae817b8d0346004b422ab
Werick <werick.luiz16@gmail.com>
2025-11-16 01:36:27 -0300

    Inicio do desenvolvimento

---
+++ src/main/java/org/example/AppGrafo.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class AppGrafo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String partida, chegada;
+
+
+        //Grafo<String> grafo = new Grafo<>(); //Cria e popula o grafo
+
+        System.out.println("\nCalculadora de rotas - Jardim Taqual");
+        System.out.println("\nDigite o ponto de partida (EX: A): ");
+        partida = scanner.nextLine();
+        System.out.println("Digite o ponto de chegada (EX: P): ");
+        chegada = scanner.nextLine();
+
+        //Aqui eu faço uma validação para saber se os pontos existem
+
+
+    }
+}
